eb11db2fd9129bb70e1dfd0d00700657d9840137
Hassan Sakkoury <hassansakkoury33@gmail.com>
2022-12-10 22:53:29 -0800

    Edit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
-#include <unistd.h>
+
 /**
  *
  *  * main - Entry point
  *
- *  * Return:1
+ *  *
  *
- *  */
-int main(void)
-{
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+ *  * Return: Always0 (Success)
+ *
+ */
 
-return (1);
+int main(void)
+
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+
+	return (1);
 }
